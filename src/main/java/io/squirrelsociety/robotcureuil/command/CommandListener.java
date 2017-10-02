7144ed70117218bfcb86bcf66dd0bdd4b1b60713
nowlow <nowlow77@gmail.com>
2017-10-02 18:51:56 +0200

    uais

--- src/main/java/io/squirrelsociety/robotcureuil/command/CommandListener.java
+++ src/main/java/io/squirrelsociety/robotcureuil/command/CommandListener.java
@@ -1,8 +1,14 @@
 package io.squirrelsociety.robotcureuil.command;
 
+import java.awt.Color;
+
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 
 /**
@@ -29,9 +35,28 @@ public class CommandListener implements EventListener {
             if(commandMap.commandUser(event.getAuthor(), message, event.getMessage())) {
                 if(event.getTextChannel() != null && event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE)) {
                     event.getMessage().deleteMessage().queue();
-
                 }
             }
+        }
+    }
+    
+    private void onReactionAdd(MessageReactionAddEvent event) {
+        Message message = event.getChannel().getMessageById(event.getMessageId()).complete();
+        System.out.println("auteur " + message.getAuthor().getName());
+        if(message.getAuthor().getName().equals("RobotCureuil")) {
+            if(message.getContent().equals("Clique sur le 🐿  pour avoir le rôle !")) {
+                System.out.println("Reaction added");
+                if(event.getUser().equals(event.getJDA().getSelfUser()))return;
+                if(event.getReactionEmote().getName().equals("🐿")) {
+                    if(event.getMember().getEffectiveName().endsWith("cureuil")) {
+                        System.out.println("Role to add : " + event.getGuild().getRoleById("364102814076633092").getName());
+                        event.getGuild().getController().addRolesToMember(event.getMember(), event.getGuild().getRoleById("364102814076633092")).queue();
+                        System.out.println("Role added to " + event.getMember().getEffectiveName());
+                    }
+                    else errorBuilder(event.getTextChannel(), "Votre pseudo ne finit pas par `cureuil`");
+                }
+            }
+           
         }
     }
 }
